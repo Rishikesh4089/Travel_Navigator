73a50ec397adbf0898c4940eefeaebb76ac20725
Rishikesh Prabhu <110054654+Rishikesh4089@users.noreply.github.com>
2023-09-27 14:49:20 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 using namespace std;
 
+class node{
+  public:
+  node *createNode(){
+    node *temp;
+    temp = new node;
+    temp=NULL;
+    return temp;
+};
+
+class Details{
+};
+
+class Travel{
+};
+
 int main(){
   cout<<"Hello World\n";
 }
